5e4da14c39277e9021c65658288e3be87d6f7de2
zhouwenkang <zhouwenkang@meituan.com>
2018-09-04 22:50:37 +0800

    MODIFY: modify some jest config add moduleName

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     setupFiles: ['./jest.setup.js'],
     rootDir: '',
-    testMatch: ['<rootDir>/__tests__/*.test.js','<rootDir>/__tests__/**/*.test.js'],
+    testMatch: ['<rootDir>/__test__/*.test.js','<rootDir>/__test__/**/*.test.js'],
     coverageDirectory: '<rootDir>/.coverage/',
     collectCoverageFrom: [
-      '**/*.js',
+      '**/*.{js,jsx}',
+      '!**/*.{config,setup}.js',
       '!**/node_modules/**',
       '!**/*.test.js',
     ],
@@ -24,14 +25,15 @@ module.exports = {
     },
     verbose: true,
     transform: {
-        '^.+\\.js$': 'babel-jest',
-        "^.+\\.[(sc)|(c)]ss$": "<rootDir>/__test__/__mocks__/css-transform.js",
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__test__/__mocks__/fileMock.js",
+        '^.+\\.js$': 'babel-jest'
     },
     moduleNameMapper: {
     //   '\\.(css|less|s(c|a)ss)$': '<rootDir>/../__mocks__/style.js',
     //   'services(.*)$': '<rootDir>/services$1',
         "^react$": "<rootDir>/node_modules/react",
+        "^.+\\.scss$": "<rootDir>/__test__/__mocks__/css-transform.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__test__/__mocks__/fileMock.js",
+        "^@app(.*)$": "<rootDir>/src/components$1",
     },
     testURL: "http://localhost/",
   };
