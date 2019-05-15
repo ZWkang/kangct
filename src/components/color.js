37af43359c7fdc50985817344ef6c18fcc7d07ba
ZWkang <907747874@qq.com>
2019-05-16 02:06:20 +0800

    ADDED: color.js

---
+++ src/components/color.js
@@ -0,0 +1,15 @@
+const colorPennel = {
+  primary: '#cce5ff',
+  secondary: '#e2e3e5',
+  success: '#d4edda',
+  danger: '#f8d7da',
+  warning: '#fff3cd',
+  info: '#d1ecf1',
+  light: '#fefefe',
+  dark: '#d6d8d9'
+}
+
+
+export default {
+  colorPennel
+}
