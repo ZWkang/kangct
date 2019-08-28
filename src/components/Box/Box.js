cbe6f97d6df9abc46f371f1eec45538f82dcd1e0
ZWkang <907747874@qq.com>
2019-08-28 19:14:05 +0800

    ADDED: Box component

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import is from 'styled-is'
-import styled from 'styled-components'
+import React from 'react';
+import is from 'styled-is';
+import styled from 'styled-components';
 
 const BoxDefaultCssString = styled.div`
   color: #4a4a4a;
   background-color: hsl(0, 0%, 100%);
   border-radius: 6px;
-  box-shadow: 0 1px 1px rgba(10,10,10,.1), 0 0 0 1px rgba(10, 10, 10, .1);
+  box-shadow: 0 1px 1px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
   padding: 1.25rem;
-  transition: all .2s ease-in;
-  cursor: pointer;
+  transition: all 0.2s ease-in;
   text-align: left;
 
   ${is('hoverStyle')`
@@ -18,12 +17,12 @@ const BoxDefaultCssString = styled.div`
       box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 0 0 2px rgba(0, 0, 0, 0.1);
     }
   `}
-`
+`;
 
 const Box = styled(BoxDefaultCssString)`
   ${is('customStyle')`
-    ${props => props.customStyle}
+    ${(props) => props.customStyle}
   `}
-`
+`;
 
-export default Box
+export default Box;
