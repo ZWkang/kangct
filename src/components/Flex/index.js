a8d7839afa6e81c6c49f1027c8f90c87e5e9d06e
ZWkang <907747874@qq.com>
2019-08-28 19:16:50 +0800

    ADDED: add flex component

--- src/components/Flex/index.js
+++ src/components/Flex/index.js
@@ -1,8 +1,12 @@
 import Flex from './Flex'
 import FlexItem from './FlexItem'
 import SimpleFlex from './SimpleFlex'
+
+
 Flex.Item = FlexItem
 Flex.SimpleFlex = SimpleFlex
+
+
 export {
   FlexItem,
   Flex as default
