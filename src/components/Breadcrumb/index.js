17479007c7bfde1a8e9eeaef2d8b43296fa77a45
ZWkang <907747874@qq.com>
2019-08-28 19:14:23 +0800

    ADDED: Breadcrumb Component

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -1,22 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
-import is from 'styled-is'
+import Breadcrumb from './Breadcrumb';
 
-const BreadcrumbDefaultCssString = `
-  font-size: 16px;
-  white-space: nowrap;
-  user-select: none;
-  display: block;
-  ul,
-  ol{
-    align-items: flex-start
-    display: flex
-    flex-wrap: wrap
-    justify-content: flex-start
-  }
-`
-const BreadcrumbWrapper = styled.div`
-  ${BreadcrumbDefaultCssString}
-`
+// 面包屑 链接以有序列表构成  nav为Breadcrumb元素标记为面包屑跟踪
 
-export default BreadcrumbWrapper
+export { Breadcrumb as default };
