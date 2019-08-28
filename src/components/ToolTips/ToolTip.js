39b1a37c86e414177cb2cd92703f0fad72a4264c
ZWkang <907747874@qq.com>
2019-08-28 19:20:02 +0800

    ADDED: ToolTips component

--- src/components/ToolTips/ToolTip.js
+++ src/components/ToolTips/ToolTip.js
@@ -1,7 +1,115 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import is from 'styled-is'
+import React, { Component, createRef } from 'react';
+import is from 'styled-is';
+import styled from 'styled-components';
+import Triangle from './Triangle';
+import Rect from '../Rect';
+import Protal from '../Protal';
+import TooltipsWrapper from './TooltipsWrapper';
 
-/**
- * tooltips 
- */
+const varTrans = (name) => (props) =>
+  `
+    ${name}: ${props[name]}px;
+  `;
+
+const Position = styled.div`
+  ${is('left')`
+    ${varTrans('left')}
+  `}
+  ${is('right')`
+    ${varTrans('right')}
+  `}
+  ${is('top')`
+    ${varTrans('top')}
+  `}
+  ${is('bottom')`
+    ${varTrans('bottom')}
+  `}
+  position: absolute;
+  transform: translateX(-50%);
+`;
+
+const Tool = styled.div`
+  position: absolute;
+  width: auto;
+  
+  ${is('top')`
+    bottom: 0;
+    transform: translateX(-50%);
+  `}
+  ${is('right')`
+    left: 0;
+    transform: translateY(-50%);
+  `}
+  ${is('left')`
+    right: 0;
+    transform: translateY(-50%);
+  `}
+  ${is('bottom')`
+    top: 0;
+    transform: translateX(-50%);
+  `}
+`;
+class ToolTips extends Component {
+  static defaultProps = {
+    delay: 0,
+    defaultVisible: false,
+    target: React.createRef(),
+    position: 'top'
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      target: React.createRef()
+    };
+    this.btnRef = createRef();
+  }
+  renderToolTipsPosition = ({ left, top, width, height }) => {
+    const { position } = this.props;
+    switch (position) {
+      case 'left':
+        left = left;
+        top = top + height / 2;
+        break;
+      case 'right':
+        left = left + width;
+        top = top + height / 2;
+        break;
+      case 'top':
+        left = left + width / 2;
+        top = top;
+        break;
+      case 'bottom':
+        left = left + width / 2;
+        top = top + height;
+        break;
+      default:
+        left = 0;
+        top = 0;
+        break;
+    }
+    const propss = { [position]: true };
+    return (
+      <Position left={left} top={top}>
+        <Tool {...propss}>
+          <Triangle {...propss}>{this.props.children}</Triangle>
+        </Tool>
+      </Position>
+    );
+  };
+  render() {
+    const { show, target } = this.props;
+    if (!show) {
+      return null;
+    }
+    return (
+      <Protal>
+        <Rect target={target} show={show}>
+          {this.renderToolTipsPosition}
+        </Rect>
+      </Protal>
+    );
+  }
+}
+
+export default ToolTips;
