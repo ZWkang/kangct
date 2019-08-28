117473028729c04e1a7a7b578fb070eb1607d10d
ZWkang <907747874@qq.com>
2019-08-28 19:20:02 +0800

    ADDED: ToolTips component

--- src/components/ToolTips/index.js
+++ src/components/ToolTips/index.js
@@ -1,39 +1,6 @@
-import React, { Component } from 'react'
-import is from 'styled-is'
-import styled, { css } from 'styled-components'
-import Triangle from './Triangle'
+import ToolTip from './ToolTip';
 
-const ToolTipsWrapper = styled.div`
-  position: relative;
-`
-
-
-
-// const ToopTips = (props) => {
-
-class ToopTips extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      toolTipShow: false
-    }
-  }
-  handleClickAction = () => {
-    this.setState(prevState => ({
-      toolTipShow: !prevState.toolTipShow
-    }))
-  }
-  render() {
-    const { toolTipShow } = this.state
-    const Content = React.cloneElement(this.props.children, {
-      onClick: this.handleClickAction
-    })
-    console.log(this.props.children)
-      return <ToolTipsWrapper>
-      { Content }
-      { toolTipShow ? <Triangle rorate={this.props.rorate}>asdasd</Triangle> : null }
-    </ToolTipsWrapper>
-  }
-}
-
-export default ToopTips
+/**
+ * @components
+ */
+export default ToolTip;
