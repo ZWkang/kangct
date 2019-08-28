ad2b188a2b5cac4b7b1aad888d4f2aad9ef8ed9a
ZWkang <907747874@qq.com>
2019-08-28 19:16:38 +0800

    fix: typo and format

--- src/components/ErrorBoundaries/index.js
+++ src/components/ErrorBoundaries/index.js
@@ -1,56 +1,54 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
 
-import styled from 'styled-components'
-
-
-const defaultCallback = function (a, b, c) {
-    throw b
-}
+const defaultCallback = function(a, b, c) {
+  throw b;
+};
 
 const DefaultFallBackComponent = ({ error, componentStack }) => {
-    {console.log(error)}
-    return <div>
-    <div style={{color: 'red'}}>
-            {error.message}
-        </div>
-        <div style={{color: 'green'}}>
-        {console.log(componentStack)}
-        {componentStack.join('\n')}
-        </div>
+  componentStack = componentStack.split('\n');
+  return (
+    <div>
+      <div style={{ color: 'red' }}>{error.message}</div>
+      <div style={{ color: 'green' }}>
+        {componentStack.map((stack, index) => {
+          return <p key={stack + index}>{stack}</p>;
+        })}
+      </div>
     </div>
-}
+  );
+};
 
 class ErrorBoundaries extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            error: null,
-            info: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      info: null
+    };
+  }
+  componentDidCatch = (error, info) => {
+    const { ErrorCallback = defaultCallback } = this.props;
+    try {
+      ErrorCallback(this, error, info);
+    } catch (err) {
+      this.setState({
+        error: err,
+        info
+      });
     }
-    componentDidCatch = (error, info) => {
-        const { ErrorCallback = defaultCallback } = this.props
-        try {
-            ErrorCallback(this, error, info)
-        } catch (err) {
-            this.setState({
-                error: err,
-                info
-            })
-        }
+  };
+  render() {
+    const { children, FallBackComponent = DefaultFallBackComponent, ...restProps } = this.props;
+    const { error, info } = this.state;
+    if (error) {
+      return <FallBackComponent error={error} componentStack={info.componentStack} />;
     }
-    render() {
-        const { children, FallBackComponent = DefaultFallBackComponent, ...restProps } = this.props
-        const { error, info } = this.state
-        if (error) {
-            return (<FallBackComponent error={error} componentStack={info.componentStack}/>)
-        }
-        return <Fragment>{children}</Fragment>
-    }
+    return <Fragment>{children}</Fragment>;
+  }
 }
 /**
  * you can write some in here you know?
- * 
- * @component
+ *
+ * @components
  */
-export default ErrorBoundaries
+export default ErrorBoundaries;
