18fe022b0f6e00feb66447f9991df018924172ff
ZWkang <907747874@qq.com>
2019-08-28 19:27:37 +0800

    ADDED: two component tests

---
+++ __test__/Accordion/Accordion.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Accordion, { Collapse } from '@app/Accordion/index.js';
+import 'jest-styled-components';
+
+describe('Accordion', () => {
+  it('Accordion accordion props default case ', () => {
+    const AccordionActiveKey = mount(
+      <Accordion accordion>
+        <Accordion.Pannel header="SECTION 1">module 1</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 2">module 2</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 3">module 3</Accordion.Pannel>
+      </Accordion>
+    );
+    const firstItem = AccordionActiveKey.find(Accordion).at(0);
+    expect(firstItem.props().activeKey).toEqual([]);
+    expect(firstItem.state().activeKey).toEqual([]);
+  });
+  it('Accordion accordion control active key', () => {
+    const AccordionActiveKey = mount(
+      <Accordion accordion activeKey={1}>
+        <Accordion.Pannel header="SECTION 1">module 1</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 2">module 2</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 3">module 3</Accordion.Pannel>
+      </Accordion>
+    );
+    const firstItem = AccordionActiveKey.find(Accordion).at(0);
+    expect(firstItem.props().activeKey).toEqual(1);
+    expect(firstItem.state().activeKey).toEqual([1]);
+
+    AccordionActiveKey.find(Collapse.ItemHeader)
+      .at(2)
+      .simulate('click');
+
+    expect(firstItem.state().activeKey).toEqual([2]);
+  });
+
+  it('Accordion accordion control muti key', () => {
+    const AccordionActiveKey = mount(
+      <Accordion activeKey={1}>
+        <Accordion.Pannel header="SECTION 1">module 1</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 2">module 2</Accordion.Pannel>
+        <Accordion.Pannel header="SECTION 3">module 3</Accordion.Pannel>
+      </Accordion>
+    );
+    const firstItem = AccordionActiveKey.find(Accordion).at(0);
+    expect(firstItem.props().activeKey).toEqual(1);
+    expect(firstItem.state().activeKey).toEqual([1]);
+
+    AccordionActiveKey.find(Collapse.ItemHeader)
+      .at(2)
+      .simulate('click');
+
+    expect(firstItem.state().activeKey).toEqual([1, 2]);
+  });
+});
--- __test__/Button/Button.test.js
+++ __test__/Button/Button.test.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import Button from '../../src/components/Button/index.js'
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Button from '@app/Button/index.js';
+import 'jest-styled-components';
 
-describe('Button Test Cases', () => {
-    it('button should be render', ()=>{
-        const wrapper = mount(<Button />)
-        expect(wrapper.text()).toEqual('content : here is my name ')
-        console.log(wrapper.text())
-    })
-})
+describe('Button', () => {
+  describe('bahavie test', () => {
+    it('button should be render', () => {
+      const wrapper = mount(<Button>content : here is my name </Button>);
+      expect(wrapper.text()).toEqual('content : here is my name ');
+    });
+
+    it('expect render element type should be <button>', () => {
+      const wrapper = mount(
+        <div>
+          <Button>test</Button>
+        </div>
+      );
+      // console.log(wrapper);
+      expect(wrapper.find('button').length).toEqual(1);
+    });
+  });
+
+  describe('useage test', () => {
+    it('it should pass props about onClick', () => {
+      const handleOnClick = jest.fn();
+      const wrapper = shallow(<Button onClick={handleOnClick}>test</Button>);
+      wrapper.simulate('click');
+      expect(handleOnClick).toBeCalled();
+    });
+    it('it should not emit onclick in disabled mode', () => {
+      const handleOnClick = jest.fn();
+      const wrapper = mount(
+        <Button disabled={true} onClick={handleOnClick}>
+          test
+        </Button>
+      );
+      wrapper.simulate('click');
+      expect(handleOnClick).not.toBeCalled();
+    });
+    it('size should be work', () => {
+      const mediumBtn = mount(<Button medium />);
+      const bigBtn = mount(<Button big />);
+      expect(mediumBtn).not.toHaveStyleRule('font-size', '1em');
+      expect(bigBtn).not.toHaveStyleRule('font-size', '1em');
+    });
+  });
+});
