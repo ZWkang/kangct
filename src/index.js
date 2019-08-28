c134db1a8e8192a22e1e86766024479a29deabef
ZWkang <907747874@qq.com>
2019-08-28 19:30:12 +0800

    ADDED: all components export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import Button from '@app/Button'
+export * from './components/Accordion';
+export * from './components/Alert';
+export * from './components/Animate';
 
-export default { Button }  
+export * from './components/Badge';
+export * from './components/borderMagic';
+export * from './components/Box';
+export * from './components/Breadcrumb';
+export * from './components/Button';
+export * from './components/Card';
+export * from './components/Column';
+export * from './components/Dialog';
+// export * from './components/Dropdown';
+// export * from './components/Dialog';
+export * from './components/Dropdown';
+// export * from './components/Component';
+export * from './components/ErrorBoundaries';
+export * from './components/Flex';
+export * from './components/Heading';
+export * from './components/Hero';
+export * from './components/ImageComponent';
+export * from './components/Lifecycle';
+
+export * from './components/Loading';
+
+export * from './components/Notification';
+export * from './components/Pagination';
+export * from './components/Pannel';
+export * from './components/progress';
+export * from './components/Protal';
+export * from './components/Rect';
+export * from './components/ScrollTo';
+export * from './components/Tab';
+export * from './components/Table';
+export * from './components/Toggle';
+export * from './components/ToolTips';
+
+// export * from './components/Base';
+// export * from './components/Page';
