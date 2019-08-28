caede12ff98c9359fea5dab661641db234385144
ZWkang <907747874@qq.com>
2019-08-28 19:17:12 +0800

    ADDED: Hero component

---
+++ src/components/Hero/HeroBody.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import is from 'styled-is';
+// class HeroBody extends PureComponent {
+//   constructor(props) {}
+//   render() {
+//       return (
+
+//       )
+//   }
+// }
