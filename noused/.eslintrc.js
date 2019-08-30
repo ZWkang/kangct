4c62f192a3020f256e60acb10f3a4efec6696a40
ZWkang <907747874@qq.com>
2019-08-30 17:07:37 +0800

    ADDED: noused file

---
+++ noused/.eslintrc.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/all', 'airbnb'],
+//   parser: 'babel-eslint',
+//   plugins: ['react'],
+//   env: {
+//     jest: true
+//   },
+//   rules: {
+//     'react/jsx-uses-react': 1,
+//     'react/jsx-uses-vars': 1,
+//     'template-curly-spacing': 'off',
+//     indent: 'off',
+//     'no-nested-ternary': 1
+//   }
+// };
