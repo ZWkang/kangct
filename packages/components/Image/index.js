f1555d0f6c0c056de53c2aa967a0ed680af637ec
ZWkang <907747874@qq.com>
2019-08-30 16:38:22 +0800

    Release v1.0.0

--- packages/components/Image/index.js
+++ packages/components/Image/index.js
@@ -1,33 +1,57 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import { injectCustomStyle } from '../utils.js'
-import is from 'styled-is'
-import MD from 'react-icons/md'
-import PropTypes from 'prop-types'
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { injectCustomStyle } from '../utils.js';
+import is from 'styled-is';
+import MD from 'react-icons/md';
+import PropTypes from 'prop-types';
 
+const fixedStyleRegExp = /\Bis(\d+?)x(\d+)\b/;
 
+const ratioStyleRegExp = /\b(\d+?)by(\d+)\b/;
 
-const handleFixedSize = value => {
-  if (!/^\d+by\d+$/.test(value)) return "100%";
+const CalculationFixedSize = (props) => {
+  const mapsKeys = Object.keys(props);
+  const filterKeys = mapsKeys.filter(fixedStyleRegExp.test);
 
-  const arr = value.split("by");
-  const [width, height] = arr;
-  return (height / width) * 100 + "%;";
+  const LastOneKey = filterKeys[filterKeys.length - 1];
+  const [, width, height] = fixedStyleRegExp.exec(LastOneKey);
+
+  return css`
+    width: ${width}px;
+    height: ${height}px;
+  `;
 };
 
-const NormalSize = css`
-  height: ${props => (props.normalSize ? props.normalSize : "12")}px;
-  width: ${props => (props.normalSize ? props.normalSize : "12")}px;
-`;
+const CalculationRatioSize = (props) => {
+  const mapsKeys = Object.keys(props);
+  const filterKeys = mapsKeys.filter(ratioStyleRegExp.test);
 
-const fixedStyle = css`
-  padding: ${props => `${handleFixedSize(props.fixedValue)}` || "100%;"};
-`;
+  const LastOneKey = filterKeys[filterKeys.length - 1];
+  const [, width, height] = ratioStyleRegExp.exec(LastOneKey);
+
+  const ratio = (height / width) * 100;
+  return css`
+    padding-bottom: ${ratio}%;
+  `;
+};
+
+const CalculationRadius = (props) => {
+  if (!props.radius) return '';
+  if (typeof props.radius === 'number') {
+    return css`
+      border-radius: ${props.radius}px;
+    `;
+  }
+  return css`
+    border-radius: 999999px;
+  `;
+};
 
 const ImageContainer = styled.figure`
   display: block;
   position: relative;
-  ${props => (props.isfixed ? fixedStyle : NormalSize)}
+  ${CalculationFixedSize}
+  ${CalculationRatioSize}
 `;
 
 const fullPosition = css`
@@ -37,33 +61,30 @@ const fullPosition = css`
   right: 0;
   top: 0;
   height: 100%;
-  height: 100%;
+  /* width: 100%; */
+  /* max-width: 100%; */
 `;
 const normalSizes = css`
   display: block;
   max-width: 100%;
+  width: 100%;
   height: auto;
 `;
 
 const ImageItem = styled.img`
-    ${props => (props.isfixed ? fullPosition : normalSizes)}
-    border-radius: ${props => (props.round ? props.round + 'px' : "9999999px")};
+    ${normalSizes}
+    ${is(ratioStyleRegExp)`
+      ${fullPosition}
+    `}
+    ${CalculationRadius}
 `;
 
+const Image = ({ radius, imageUrl, ...reset }) => {
+  return (
+    <ImageContainer {...rest}>
+      <ImageItem radius={radius} src={imageUrl} {...rest} />
+    </ImageContainer>
+  );
+};
 
-
-export default function Image(props) {
-    const { imageUrl, isfixed=false, normalSize = {"128"}, round = 0 } = props;
-    return (
-      <ImageContainer isfixed={isfixed} normalSize fixedValue={"12by1"}>
-        <ImageItem src={imageUrl} isfixed={isfixed} />
-      </ImageContainer>
-    );
-}
-
-Image.propTypes = {
-    imageUrl: PropTypes.string.isRequired,
-    isfixed: PropTypes.bool,
-    normalSize: PropTypes.string,
-    round: PropTypes.number.isRequired
-}
+export default Image;
