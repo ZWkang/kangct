b45a30036a1cc6ce8c3bd9e977a373c1f690c553
ZWkang <907747874@qq.com>
2019-08-30 16:38:22 +0800

    Release v1.0.0

--- src/components/scrollTo/index.js
+++ src/components/scrollTo/index.js
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
-import jump from 'jump.js'
+import React, { Component } from 'react';
+import jump from 'jump.js';
 
-
-export default function (){
-  return <React.Fragment>
-    
-  </React.Fragment>
+class ScrollTo extends Component {
+  handleScroll = (options) => {
+    const { selector, handleScroll } = this.props;
+    const target = document.querySelector(selector);
+    options.callback = () => {
+      handleScroll && handleScroll();
+    };
+    target && jump(target, options);
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.children({
+          handleScroll: this.handleScroll,
+          ...this.props
+        })}
+      </React.Fragment>
+    );
+  }
 }
+
+ScrollTo.defaultProps = {};
+/**
+ * @components
+ */
+export default ScrollTo;
