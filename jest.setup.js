16c25bd9ad14d38da3fdb75d9fca3c608d8a5050
ZWkang <907747874@qq.com>
2019-09-22 15:16:59 +0800

    test: test env global setting

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,10 @@
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-
+const React = require('react');
 enzyme.configure({ adapter: new Adapter() });
 
 global.shallow = enzyme.shallow;
 global.mount = enzyme.mount;
 global.render = enzyme.render;
+
+global.React = React;
