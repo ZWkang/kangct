0354df4778494e52530f49bff471f10ba67a0850
ZWkang <kang95630@gmail.com>
2021-05-16 05:30:18 +0800

    fix: util typo

--- src/components/util/index.js
+++ src/components/util/index.js
@@ -29,7 +29,7 @@ export function tinyDate(str) {
   let hashKeys = {
     YYYY: 'getFullYear',
     YY: 'getYear',
-    MM: function(d) {
+    MM: function (d) {
       return d.getMonth() + 1;
     },
     DD: 'getDate',
@@ -51,7 +51,7 @@ export function Object_omit(obj, props) {
   return result;
 }
 
-export const transfromData = ({ imageUrl }) => {
+export const transformData = ({ imageUrl }) => {
   //   console.log(imageUrl);
 
   return new Promise((resolve, reject) => {
@@ -61,8 +61,8 @@ export const transfromData = ({ imageUrl }) => {
       var image = new Image();
       image.src = imageUrl;
       image.crossOrigin = 'Anonymous';
-      const arra = [];
-      image.onload = function() {
+      const arr = [];
+      image.onload = function () {
         //将图片按像素写入画布
         context.drawImage(image, 0, 0, this.width, this.height);
         //读取图片某位置像素信息(x,y为需要读取的像素位置)
@@ -72,12 +72,12 @@ export const transfromData = ({ imageUrl }) => {
           for (var j = 0; j < this.height; j++) {
             const data = context.getImageData(i, j, 1, 1).data;
             const result = [i, j, `rgba(${data[0]},${data[1]},${data[2]},${data[3] / 255})`];
-            arra.push(result);
+            arr.push(result);
           }
         }
-        resolve(arra);
+        resolve(arr);
       };
-      image.onerror = function(e) {
+      image.onerror = function (e) {
         reject(e);
       };
     });
