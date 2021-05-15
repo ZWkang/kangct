c717b7410d5ab24a0566dd427c6215b3c7e0e077
ZWkang <kang95630@gmail.com>
2021-05-16 05:09:12 +0800

    fix: typo and unuse deps

--- src/components/Flex/FlexItem.js
+++ src/components/Flex/FlexItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import is from 'styled-is';
 import { FlexItemPropsTypes, FlexCommonProps } from './props';
 
--- src/components/Flex/SimpleFlex.js
+++ src/components/Flex/SimpleFlex.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import is from 'styled-is';
 
 export default styled.div`
   display: flex;
