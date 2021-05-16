247e05ab6082bfbd466c111998a68950133b6664
ZWkang <kang95630@gmail.com>
2021-05-16 17:53:00 +0800

    gh-pages updated 2021-05-16 17:53:00

---
+++ dist/components/Panel/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _Panel = require('./Panel');
+
+var _Panel2 = _interopRequireDefault(_Panel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Panel2.default;
